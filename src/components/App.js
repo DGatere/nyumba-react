0efd7b3193056aa0857f7e94aaca909fce91354c
dgatere <dennisgatere@gmail.com>
2019-10-26 20:54:13 +0300

    Add Layout component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Header from './Header/Header';
+import Layout from './Layout/Layout';
 
 const App = () => {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <Layout />
+        </>
+    );
 };
 
 export default App;
