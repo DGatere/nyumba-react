21c4fe6df3669ea8551641bfc30bf39a571c9c84
dgatere <dennisgatere@gmail.com>
2019-10-26 22:26:07 +0300

    Make styling globally accessible via index.css and clean up css variables and import statements

--- src/components/Layout/Products/Products.js
+++ src/components/Layout/Products/Products.js
@@ -6,7 +6,7 @@ import FeaturedContent from './Featured/Featured';
 
 const Products = () => {
     return (
-        <>
+        <section>
             <div className='featured-product__content'>
                 <h3 className='featured-product__header'>Featured Properties</h3>
                 <h5 className='featured-product__header_grey'>Find properties around your city</h5>
@@ -17,7 +17,7 @@ const Products = () => {
                 <h3 className='featured-product__header'>Recently Added Properties</h3>
             </div>
             <FeaturedContent />
-        </>
+        </section>
     );
 };
 
