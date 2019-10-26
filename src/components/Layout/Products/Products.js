4a5fca23ddca3c8d8aad838b8d4848ae52e963da
dgatere <dennisgatere@gmail.com>
2019-10-26 22:15:03 +0300

    Fix invalid class usage

--- src/components/Layout/Products/Products.js
+++ src/components/Layout/Products/Products.js
@@ -7,14 +7,14 @@ import FeaturedContent from './Featured/Featured';
 const Products = () => {
     return (
         <>
-            <div class='featured-product__content'>
-                <h3 class='featured-product__header'>Featured Properties</h3>
-                <h5 class='featured-product__header_grey'>Find properties around your city</h5>
+            <div className='featured-product__content'>
+                <h3 className='featured-product__header'>Featured Properties</h3>
+                <h5 className='featured-product__header_grey'>Find properties around your city</h5>
             </div>
             <Navigation />
             <FeaturedContent />
-            <div class='featured-product__content'>
-                <h3 class='featured-product__header'>Recently Added Properties</h3>
+            <div className='featured-product__content'>
+                <h3 className='featured-product__header'>Recently Added Properties</h3>
             </div>
             <FeaturedContent />
         </>
