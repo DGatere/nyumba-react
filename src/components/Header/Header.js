bcb41715c660d0c9ac94d8810c9f85326754e24f
dgatere <dennisgatere@gmail.com>
2019-10-31 21:37:43 +0300

    - Refactor Header and move Toolbar to it own component and forward onclick props - Move toolbar related styling and add dynamic behavior for logo placement for mobile and large screens (on the left desktop on the right in mobile)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,41 @@
 import React from 'react';
 
 import './Header.css';
+import Toolbar from './Toolbar/Toolbar';
 
-const Header = () => {
-    return (
-        <header className='header'>
-            <div>
-                <button className='toggle-button'>
-                    <span className='toggle-button__bar'></span>
-                    <span className='toggle-button__bar'></span>
-                    <span className='toggle-button__bar'></span>
-                </button>
-                <a href='# '>
-                    <img src={require('../../assets/images/nyumba_logo.png')} alt='logo' />
-                </a>
-            </div>
-            <nav className='header-nav'>
-                <ul className='header-nav__items'>
-                    <li className='header-nav__item'>
-                        <a href='# ' className='header-nav__link_grey'>
-                            Hot Properties
-                        </a>
-                    </li>
-                    <li className='header-nav__item'>
-                        <a href='# ' className='header-nav__link_grey'>
-                            View Properties
-                        </a>
-                    </li>
-                    <li className='header-nav__item '>
-                        <a href='# ' className='header-nav__link_golden-border'>
-                            Be A Partner
-                        </a>
-                    </li>
-                    <li className='header-nav__item'>
-                        <a href='# ' className='header-nav__link_theme-gold'>
-                            Sell Property
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
-};
+const Header = props => (
+    <header className='header'>
+        <Toolbar click={props.toggle} />
+        <div className='logo'>
+            <a href='# '>
+                <img src={require('../../assets/images/nyumba_logo.png')} alt='logo' />
+            </a>
+        </div>
+        <nav className='header-nav'>
+            <ul className='header-nav__items'>
+                <li className='header-nav__item'>
+                    <a href='# ' className='header-nav__link_grey'>
+                        Hot Properties
+                    </a>
+                </li>
+                <li className='header-nav__item'>
+                    <a href='# ' className='header-nav__link_grey'>
+                        View Properties
+                    </a>
+                </li>
+                <li className='header-nav__item '>
+                    <a href='# ' className='header-nav__link_golden-border'>
+                        Be A Partner
+                    </a>
+                </li>
+                <li className='header-nav__item'>
+                    <a href='# ' className='header-nav__link_theme-gold'>
+                        Sell Property
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+);
 
 export default Header;
