cc13ee39ad18b7defc46c38a0fa6a20a72812885
dgatere <dennisgatere@gmail.com>
2019-10-31 21:40:55 +0300

    - Refactor App to a class based component to handle state of backdrop and sidedrawer components when clicked

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './Header/Header';
 import Layout from './Layout/Layout';
+import SideDrawer from './SideDrawer/SideDrawer';
+import Backdrop from './Backdrop/Backdrop';
 
-const App = () => {
-    return (
-        <>
-            <Header />
-            <Layout />
-        </>
-    );
-};
+class App extends Component {
+    state = {
+        drawerOpen: false
+    };
+
+    drawerToggleClickHandler = () => {
+        this.setState(prevState => {
+            return {
+                drawerOpen: !prevState.drawerOpen
+            };
+        });
+    };
+
+    backdropClickHandler = () => {
+        this.setState({ drawerOpen: false });
+    };
+
+    render() {
+        let backDrop;
+
+        if (this.state.drawerOpen) {
+            backDrop = <Backdrop close={this.backdropClickHandler} />;
+        }
+
+        return (
+            <>
+                <Header toggle={this.drawerToggleClickHandler} />
+                <SideDrawer show={this.state.drawerOpen} />
+                {backDrop}
+                <Layout />
+            </>
+        );
+    }
+}
 
 export default App;
