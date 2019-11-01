536c878aac69f6f2b42a6e40aed749937c005be2
dgatere <dennisgatere@gmail.com>
2019-11-01 17:54:13 +0300

    Add ProductInfo to ContentGrid container for display

--- src/containers/ContentGrid/ContentGrid.js
+++ src/containers/ContentGrid/ContentGrid.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './ContentGrid.css';
 import ImageGrid from '../../components/ProductArea/ImageGrid/ImageGrid';
 import NavigationItems from '../../components/ProductArea/NavigationItems/NavigationItems';
+import ProductInfo from '../../components/ProductArea/ProductInfo/ProductInfo';
 
 const ContentGrid = () => (
     <div className='content-grid'>
         <section className='product-area'>
             <ImageGrid />
             <NavigationItems />
+            <ProductInfo />
         </section>
     </div>
 );
