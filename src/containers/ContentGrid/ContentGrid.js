e9eb4d95b3b8df6bff6d1026e03a6ca4203cc9b7
dgatere <dennisgatere@gmail.com>
2019-11-01 22:28:21 +0300

    Add missing Profile component to page

--- src/containers/ContentGrid/ContentGrid.js
+++ src/containers/ContentGrid/ContentGrid.js
@@ -4,6 +4,7 @@ import './ContentGrid.css';
 import ImageGrid from '../../components/ProductArea/ImageGrid/ImageGrid';
 import NavigationItems from '../../components/ProductArea/NavigationItems/NavigationItems';
 import ProductInfo from '../../components/ProductArea/ProductInfo/ProductInfo';
+import Profile from '../../components/ProductArea//SideArea/Profile/Profile';
 import Recommendations from '../../components/ProductArea/SideArea/Recommendations/Recommendations';
 
 const ContentGrid = () => (
@@ -12,6 +13,7 @@ const ContentGrid = () => (
             <ImageGrid />
             <NavigationItems />
             <ProductInfo />
+            <Profile />
             <Recommendations />
         </section>
     </div>
