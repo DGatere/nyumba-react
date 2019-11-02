5cf694943b2d997b4b6902bcf08a3b35d6d8428c
dgatere <dennisgatere@gmail.com>
2019-11-02 10:13:50 +0300

    Minor style changes to footer and add temporary key for list

--- src/components/Footer/Navigation/Navigation.js
+++ src/components/Footer/Navigation/Navigation.js
@@ -6,7 +6,7 @@ import NavItems from './NavItems/NavItems';
 const navContent = [
     { metrics: '262k', description: 'listing for sales', link: 'Sell Property' },
     { metrics: '500k', description: 'listing for rent', link: 'Rent Property' },
-    { metrics: '120k', description: 'Business Partners', link: 'Be a Partner' }
+    { metrics: '120k', description: 'Business Partners', link: 'Be a partner' }
 ];
 
 const Navigation = () => {
@@ -14,7 +14,7 @@ const Navigation = () => {
         <nav>
             <ul className='footer-nav__items'>
                 {navContent.map(navItems => (
-                    <NavItems items={navItems} />
+                    <NavItems key={Math.random()} items={navItems} />
                 ))}
             </ul>
         </nav>
