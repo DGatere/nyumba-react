1e222d1543e89cf32b7ec0f785f11c47559d0a05
dgatere <dennisgatere@gmail.com>
2019-11-03 17:03:33 +0300

    Add service worker back

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+serviceWorker.register();
